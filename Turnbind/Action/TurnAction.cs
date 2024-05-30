cd4e1f852f28a7df60760bd60b50b844a97d68dc
c0nstexpr <976999484@qq.com>
2024-05-30 17:30:53 +0800

    feat(*): add mouse move factor WIP

--- Turnbind/Action/TurnAction.cs
+++ Turnbind/Action/TurnAction.cs
@@ -1,28 +1,11 @@
-﻿using System.DirectoryServices.ActiveDirectory;
-using System.Runtime.InteropServices;
-
+﻿
 using Microsoft.Extensions.Logging;
 
-using Windows.Win32;
-using Windows.Win32.UI.Input.KeyboardAndMouse;
 
 namespace Turnbind.Action;
 
 sealed partial class TurnAction : IDisposable
 {
-    static readonly int m_inputTypeSize = Marshal.SizeOf(typeof(INPUT));
-
-    readonly INPUT[] m_inputs = [
-        new()
-        {
-            type = INPUT_TYPE.INPUT_MOUSE,
-            Anonymous = new()
-            {
-                mi = new() { dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE }
-            }
-        }
-    ];
-
     readonly ILogger<TurnAction> m_log = App.GetRequiredService<ILogger<TurnAction>>();
 
     public TimeSpan Interval
@@ -31,7 +14,6 @@ sealed partial class TurnAction : IDisposable
 
         set
         {
-            m_pixelPerPeriod = m_pixelPerPeriod * (value / m_timer.Period);
             m_timer.Period = value;
             m_log.LogInformation("Turn Interval changed to {ms} ms", value.Milliseconds);
         }
@@ -45,174 +27,61 @@ sealed partial class TurnAction : IDisposable
 
         set
         {
-            m_pixelPerPeriod = m_pixelPerPeriod / m_pixelPerMs * value;
             m_pixelPerMs = value;
             m_log.LogInformation("Changed to {p} Pixels/Sec", PixelPerMs);
         }
     }
 
-    double m_pixelPerPeriod;
-
-    double m_remain;
-
-    double m_anchorX;
-
-    readonly List<TurnInstruction> m_directions = [];
+    readonly List<TurnInstruction> m_directionQueue = [];
 
     TurnInstruction m_direction;
 
     public TurnInstruction Direction
     {
-        get
-        {
-            TurnInstruction dir;
-            var token = false;
-
-            try
-            {
-                m_lock.Enter(ref token);
-                dir = m_direction;
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                if (token) m_lock.Exit();
-            }
-
-            return dir;
-        }
+        get => m_direction;
 
         private set
         {
-            var token = false;
-
             if (value == m_direction) return;
 
             m_direction = value;
-            try
-            {
-                m_lock.Enter(ref token);
-
-                void Reset()
-                {
-                    m_remain = 0;
-                    m_anchorX = m_inputAction.Point.X;
-                }
-
-                switch (value)
-                {
-                    case TurnInstruction.Left:
-                        if (m_pixelPerPeriod > 0) m_pixelPerPeriod = -m_pixelPerPeriod;
-                        Reset();
-                        break;
-
-                    case TurnInstruction.Right:
-                        if (m_pixelPerPeriod < 0) m_pixelPerPeriod = -m_pixelPerPeriod;
-                        Reset();
-                        break;
-                }
-            }
-            catch
-            {
-                throw;
-            }
-            finally
-            {
-                if (token) m_lock.Exit();
-            }
 
             m_log.LogInformation("Turn action changed to {Dir}", value);
         }
     }
 
-    readonly PeriodicTimer m_timer = new(TimeSpan.FromMilliseconds(1));
-
-    ref MOUSEINPUT m_mouseInput => ref m_inputs[0].Anonymous.mi;
-
-    public void InputDeltaX(int dx)
-    {
-        var token = false;
-
-        try
-        {
-            m_lock.Enter(ref token);
-            m_mouseInput.dx = dx;
-            m_remain = m_pixelPerPeriod + m_remain - dx;
-        }
-        catch
-        {
-            throw;
-        }
-        finally
-        {
-            if (token) m_lock.Exit();
-        }
-
-        PInvoke.SendInput(m_inputs, m_inputTypeSize);
-    }
-
-    readonly InputAction m_inputAction = App.GetRequiredService<InputAction>();
 
     public double MouseFactor { get; set; }
 
-    SpinLock m_lock;
-
-    public TurnAction()
-    {
-        m_pixelPerPeriod = m_pixelPerMs * m_timer.Period.TotalSeconds;
-        Task.Run(Tick);
-    }
+    public TurnAction() => Task.Run(Tick);
 
     public int InputDirection(TurnInstruction value)
     {
         if (value == TurnInstruction.Stop) OnStopPop();
         else
         {
-            m_directions.Add(value);
+            m_directionQueue.Add(value);
             Direction = value;
         }
 
         m_log.LogInformation("Turn action changed to {Dir}", Direction);
 
-        return m_directions.Count - 1;
+        return m_directionQueue.Count - 1;
     }
 
     public void UpdateDirection(int index, TurnInstruction value)
     {
-        m_directions[index] = value;
-        if (index == m_directions.Count - 1) OnStopPop();
+        m_directionQueue[index] = value;
+        if (index == m_directionQueue.Count - 1) OnStopPop();
     }
 
     void OnStopPop()
     {
-        while (m_directions.Count > 0 && m_directions[^1] == TurnInstruction.Stop)
-            m_directions.RemoveAt(m_directions.Count - 1);
-        Direction = m_directions.Count == 0 ? TurnInstruction.Stop : m_directions[^1];
+        while (m_directionQueue.Count > 0 && m_directionQueue[^1] == TurnInstruction.Stop)
+            m_directionQueue.RemoveAt(m_directionQueue.Count - 1);
+        Direction = m_directionQueue.Count == 0 ? TurnInstruction.Stop : m_directionQueue[^1];
     }
 
-    async void Tick()
-    {
-        while (await m_timer.WaitForNextTickAsync())
-        {
-            var token = false;
-
-            try
-            {
-                m_lock.Enter(ref token);
-
-                if (Direction == TurnInstruction.Stop)
-                    SpinWait.SpinUntil(() => Direction != TurnInstruction.Stop);
-                else InputDeltaX((int)(m_pixelPerPeriod + m_remain + MouseFactor * (m_inputAction.Point.X - m_anchorX)));
-            }
-            finally
-            {
-                if (token) m_lock.Exit();
-            }
-        }
-    }
 
     public void Dispose() => m_timer.Dispose();
 }
---
+++ Turnbind/Action/TurnTickAction.cs
@@ -0,0 +1,90 @@
+﻿using Windows.Win32;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
+using System.Runtime.InteropServices;
+
+namespace Turnbind.Action;
+class TurnTickAction : IDisposable
+{
+    static readonly int m_inputTypeSize = Marshal.SizeOf(typeof(INPUT));
+
+    readonly INPUT[] m_inputs = [
+        new()
+        {
+            type = INPUT_TYPE.INPUT_MOUSE,
+            Anonymous = new()
+            {
+                mi = new() { dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE }
+            }
+        }
+    ];
+
+    readonly PeriodicTimer m_timer = new(TimeSpan.FromMilliseconds(1));
+
+    ref MOUSEINPUT m_mouseInput => ref m_inputs[0].Anonymous.mi;
+
+    readonly InputAction m_inputAction = App.GetRequiredService<InputAction>();
+
+    async void Tick()
+    {
+        double remain;
+        double anchorX;
+
+        while (await m_timer.WaitForNextTickAsync())
+        {
+            var token = false;
+
+            if (value == TurnInstruction.Stop)
+            {
+                remain = 0;
+                anchorX = m_inputAction.Point.X;
+            }
+
+            if (Direction == TurnInstruction.Stop)
+                SpinWait.SpinUntil(() => Direction != TurnInstruction.Stop);
+            else
+            {
+
+                var m_pixelPerPeriod = m_pixelPerMs * m_timer.Period.TotalSeconds;
+
+                switch (value)
+                {
+                    case TurnInstruction.Left:
+                        if (m_pixelPerPeriod > 0) m_pixelPerPeriod = -m_pixelPerPeriod;
+                        Reset();
+                        break;
+
+                    case TurnInstruction.Right:
+                        if (m_pixelPerPeriod < 0) m_pixelPerPeriod = -m_pixelPerPeriod;
+                        Reset();
+                        break;
+                }
+
+
+                var dx = (int)(m_pixelPerPeriod + remain + MouseFactor * (m_inputAction.Point.X - anchorX));
+
+                {
+                    var token = false;
+
+                    try
+                    {
+                        m_lock.Enter(ref token);
+                        m_mouseInput.dx = dx;
+                        remain = m_pixelPerPeriod + remain - dx;
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        if (token) m_lock.Exit();
+                    }
+
+                    PInvoke.SendInput(m_inputs, m_inputTypeSize);
+                }
+            }
+        }
+    }
+
+    public void Dispose() => m_timer.Dispose();
+}
